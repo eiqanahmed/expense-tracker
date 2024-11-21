bf8d3bce34299f4b0cbc9ddd1c0b8c1338fe72f7
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2024-11-21 11:23:42 -0500

    Fixed to show only current month's revenues in RevenueChart.jsx

--- frontend/src/components/RevenueChart.jsx
+++ frontend/src/components/RevenueChart.jsx
@@ -1,18 +1,21 @@
-// src/components/ExpensePieChart.jsx
 import React from 'react';
 import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
-import { Text, Input, Button, VStack, HStack, Box } from '@chakra-ui/react'
 
 const RevenueChart = ({ data }) => {
-  const COLORS = ['#76E4F7' ,'#0BC5EA' ,'#90cdf4', '#81E6D9', '#4FD1C5', '#319795', '#285E61'].reverse();
+  const COLORS = ['#76E4F7', '#0BC5EA', '#90cdf4', '#81E6D9', '#4FD1C5', '#319795', '#285E61'].reverse();
 
-  let modifiedData = []
-  data.forEach((p) => {
-    if (p.value > 0) {
-        modifiedData.push(p);
-    }
-  })
+  // Get the current month and year
+  const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
 
+  // Filter data for the current month and year
+  const currentMonthData = data.filter((item) => {
+    const itemDate = new Date(item.date); // Ensure `date` is part of your data and is in a valid format
+    return itemDate.getMonth() === currentMonth && itemDate.getFullYear() === currentYear;
+  });
+
+  // Include only positive values
+  const filteredData = currentMonthData.filter((p) => p.value > 0);
 
   // Function to transform the data array
   const transformData = (data) => {
@@ -25,13 +28,13 @@ const RevenueChart = ({ data }) => {
       return acc;
     }, {});
 
-    return Object.keys(categoryTotals).map(category => ({
+    return Object.keys(categoryTotals).map((category) => ({
       name: category.toUpperCase(),
       value: categoryTotals[category],
     }));
   };
 
-  const transformedData = transformData(modifiedData);
+  const transformedData = transformData(filteredData);
 
   return (
     <PieChart width={400} height={400}>
@@ -51,7 +54,7 @@ const RevenueChart = ({ data }) => {
       </Pie>
       <Tooltip />
       <Legend />
-      <Legend layout="vertical" verticalAlign="bottom" align="center"/>
+      <Legend layout="vertical" verticalAlign="bottom" align="center" />
     </PieChart>
   );
 };
